20e59304da5ad84862335c3c918a261437b1e9b8
marzane <marzaneporquesi@gmail.com>
2025-01-10 22:10:18 +0100

    commit

---
+++ app/src/main/java/com/marzane/notes_app/Utils/RecyclerViewNotesManager.java
@@ -0,0 +1,71 @@
+package com.marzane.notes_app.Utils;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.marzane.notes_app.models.NoteModel;
+
+import java.util.ArrayList;
+
+public class RecyclerViewNotesManager {
+
+    private static RecyclerView rvNoteList;
+    private static ArrayList<NoteModel> arrayRecentNotes = new ArrayList<>();
+
+
+    private RecyclerViewNotesManager(){}
+
+
+    public static void deleteItemAndData(NoteModel noteModel){
+        int position = arrayRecentNotes.indexOf(noteModel);
+        if (position > -1){
+            arrayRecentNotes.remove(position);
+            rvNoteList.getAdapter().notifyItemRemoved(position);
+        }
+    }
+
+
+    public static void insertOrUpdateItem(NoteModel noteModel){
+        int position = arrayRecentNotes.indexOf(noteModel);
+        if (position > -1){ // update
+            arrayRecentNotes.set(position, noteModel);
+            rvNoteList.getAdapter().notifyItemChanged(position);
+
+        } else { // insert
+            arrayRecentNotes.add(noteModel);
+            rvNoteList.getAdapter().notifyItemInserted(arrayRecentNotes.size());
+        }
+    }
+
+
+    public static void moveItem(int to, NoteModel noteModel){
+        int from = arrayRecentNotes.indexOf(noteModel);
+
+        if(from > -1){
+            arrayRecentNotes.remove(from);
+            arrayRecentNotes.add(to, noteModel);
+
+            rvNoteList.getAdapter().notifyItemMoved(from, to);
+        }
+    }
+
+
+    public static ArrayList<NoteModel> getDataList(){
+        return arrayRecentNotes;
+    }
+
+
+    public static RecyclerView getRecyclerView(){
+        return rvNoteList;
+    }
+
+
+    public static void setDataList(ArrayList<NoteModel> arrayNotes){
+        arrayRecentNotes = arrayNotes;
+    }
+
+
+    public static void setRecyclerView(RecyclerView rv){
+        rvNoteList = rv;
+    }
+
+}
