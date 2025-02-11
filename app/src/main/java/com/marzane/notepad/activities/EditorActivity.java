a2bf4caba4f64c57f8981d3838980597b46c1cb4
marzane <marzaneporquesi@gmail.com>
2025-02-11 21:26:17 +0100

    commit

--- app/src/main/java/com/marzane/notepad/activities/EditorActivity.java
+++ app/src/main/java/com/marzane/notepad/activities/EditorActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
